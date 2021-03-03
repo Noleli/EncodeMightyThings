f7ea6ab416ea7d3790ca969da050f8af96c11867
Noah Liebman <noah@noahliebman.net>
2021-03-02 21:23:21 -0600

    draw a parachute!

--- mightyThingsEncoder.js
+++ mightyThingsEncoder.js
@@ -1,9 +1,130 @@
 class ChuteVisualizer {
     constructor(containerSelector) {
         this.container = d3.select(containerSelector);
+        this.svg = this.container.append("svg");
+        this.radialContainer = this.svg.append("g");
+        this.parachute = this.radialContainer.append("g");
+        this.margin = 2;
+        
+        this.angle = d3.scaleLinear()
+            .range([0, 2 * Math.PI]);
+        
+        this.size();
+        this.update(); // probably in the constructor for debug only
     }
     
     update() {
+        this.angle
+            .domain([0, data[0].length]); // in case the number of bits changed
+        
+        const rings = this.parachute.selectAll("g.ring").data(data);
+        rings.join("g")
+            .attr("class", "ring")
+            .each((rowData, rowIndex, rowG) => {
+                const row = d3.select(rowG[rowIndex]);
+                row.selectAll("path.segment").data(d => d)
+                    .join("path")
+                        .attr("class", "segment")
+                        .attr("d", (b, bitIndex) => this.makeArc(bitIndex, rowIndex))
+                        .attr("stroke", "#000000")
+                        .attr("fill", d => d ? "#d24a4c" : "#ffffff");
+            });
+    }
+    
+    makeArc(bitIndex, rowIndex) {
+        // inner and outer radius of each ring as a proportion of the total radius
+        const ringProportions = [[0.09, 0.37], [0.37, 0.63], [0.63, 0.80], [0.90, 1]];
+        
+        // whether or not the inner and outer edges of the rings have angled stitching
+        const stitched = [[false, true], [true, true], [true, false], [false, false]];
+        const stitchDepth = 0.04; // proportion of radius
+        
+        const innerRadius = this.radius * ringProportions[rowIndex][0];
+        const outerRadius = this.radius * ringProportions[rowIndex][1];
+        
+        // arbitrary decision: the inner edge of the stitching is on the
+        // innerRadius, and the outer edge pokes outside of it.
+        
+        // even bits stitch outward going clockwise.
+        
+        const startAngle = this.angle(bitIndex);
+        const endAngle = this.angle(bitIndex + 1);
+        
+        // define the four corner points as [angle, radius]
+        const corners = [];
+        if(bitIndex % 2) {
+            corners[0] = [startAngle, innerRadius];
+            corners[1] = [startAngle, outerRadius];
+            
+            if(stitched[rowIndex][1]) {
+                corners[2] = [endAngle, outerRadius + this.radius * stitchDepth];
+            }
+            else {
+                corners[2] = [endAngle, outerRadius];
+            }
+            
+            if(stitched[rowIndex][0]) {
+                corners[3] = [endAngle, innerRadius + this.radius * stitchDepth];
+            }
+            else {
+                corners[3] = [endAngle, innerRadius];
+            }
+        }
+        else {
+            if(stitched[rowIndex][0]) {
+                corners[0] = [startAngle, innerRadius + this.radius * stitchDepth];
+            }
+            else {
+                corners[0] = [startAngle, innerRadius];
+            }
+            
+            if(stitched[rowIndex][1]) {
+                corners[1] = [startAngle, outerRadius + this.radius * stitchDepth];
+            }
+            else {
+                corners[1] = [startAngle, outerRadius];
+            }
+            
+            corners[2] = [endAngle, outerRadius];
+            corners[3] = [endAngle, innerRadius];
+        }
+        
+        const cartesianCorners = corners.map(c => d3.pointRadial(...c));
+        
+        let pathData = `M ${cartesianCorners[0].join(" ")}
+                        L ${cartesianCorners[1].join(" ")}`;
+        
+        if(stitched[rowIndex][1]) {
+            pathData += `L ${cartesianCorners[2].join(" ")}`;
+        }
+        else {
+            pathData += `A ${outerRadius} ${outerRadius} 0 0 1 ${cartesianCorners[2].join(" ")}`;
+        }
+        
+        pathData += `L ${cartesianCorners[3]}`;
+        
+        if(stitched[rowIndex][0]) {
+            pathData += "Z";
+        }
+        else {
+            pathData += `A ${innerRadius} ${innerRadius} 0 0 0 ${cartesianCorners[0].join(" ")}`;
+        }
+        
+        return pathData;
+    }
+    
+    size() {
+        this.outerRadius = 300;
+        this.radius = this.outerRadius - this.margin;
+        
+        this.svg
+            .attr("width", 2 * this.outerRadius)
+            .attr("height", 2 * this.outerRadius);
+            
+        this.radialContainer
+            .attr("transform", `translate(${this.outerRadius}, ${this.outerRadius})`);
+        
+        
         
     }
 }
@@ -26,9 +147,11 @@ class MightyThingsEncoder {
         this.outputStrings = "";
     }
     
-    tokenize() {       
+    tokenize(inputString) {
         // turn each string into an array of characters or numbers, ignoring
         // spaces. if a number is < 128, return a number; otherwise characters.
+        if(inputString !== undefined) this.inputString = inputString;
+        
         const words = this.inputString.split(" ");
         
         const charArray = words.map(w => {
@@ -57,6 +180,8 @@ class MightyThingsEncoder {
         else {
             this.tokens = tokens;
         }
+        
+        return tokens;
     }
     
     binEncoder(token) {
@@ -79,12 +204,19 @@ class MightyThingsEncoder {
         return binVal.join("");
     }
     
-    encode() {
+    encode(inputString) {
+        if(inputString !== undefined) {
+            this.inputString = inputString;
+        }
         this.tokenize(this.inputString);
         return this.tokens.map(t => this.binEncoder(t));
     }
     
-    encodePadded(row) {
+    encodePadded(row, inputString) {
+        if(inputString !== undefined) {
+            this.inputString = inputString;
+        }
+        
         const encoded = this.encode(this.inputString);
         
         // pad between each byte
@@ -103,21 +235,21 @@ class MightyThingsEncoder {
             padded += this.padChar;
         }
         
-        console.log(padded);
         // rotate the array so it starts on the right place based on the row
         padded = padded.split("").map((v, i, ar) => {
             let shiftedIndex = (i - this.startBits[row]) % ar.length;
             shiftedIndex += shiftedIndex < 0 ? ar.length : 0;
             return ar[shiftedIndex];
-        }).join("");
+        });
         
-        return padded;
+        return padded.map(d => d === "1");
     }
 }
 
-const inputStrings = ["DARE", "MIGHTY", "THINGS", "34 11 58 N 118 10 31 W"];
-
+const strings = ["DARE", "MIGHTY", "THINGS", "34 11 58 N 118 10 31 W"];
 const encoder = new MightyThingsEncoder();
-encoder.inputString = inputStrings[0];
+const data = strings.map((s, i) => encoder.encodePadded(i, s));
+const vis = new ChuteVisualizer("#chuteContainer");
 
-console.log(encoder.encodePadded(0));
+console.log(data);
+
